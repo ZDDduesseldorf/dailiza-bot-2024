fc9d68a2ecc251ddfeafd55495c21549b4c8b2f1
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2024-10-29 13:26:17 +0100

    added regex pattern (ich will ...)

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -67,3 +67,6 @@ psychobabble.append((r'ich mag (.*)', ["Warum magst du {0}?", "Was gefällt dir 
 
 
 psychobabble.append((r'(?:H|h)ast du (.*?)(?=\?|$)', ["Nein. Hast du {0}?", "Vielleicht habe ich {0}, ich bin mir nicht sicher... Hast du {0}?", "Ja, ich habe {0}, du auch?"]))
+
+
+psychobabble.append((r'(?:I|i)ch will ([^?]*?)(?:[!.]*$|$)', ["Das heißt ich moechte.\n...\nWas nicht heisst, dass es auch passiert.\nUnd jetzt?", "Ich will auch {0}, aber das Leben ist nunmal kein Wunschkonzert.\nGibt's irgendwas anderes das ich tun kann? Und bitte, bleib realistisch.", "Du willst {0}? Naja, muss ja jeder selber wissen.\nSonst noch was?"]))
