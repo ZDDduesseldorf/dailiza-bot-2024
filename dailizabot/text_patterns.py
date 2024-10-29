0cf7f70c36e2bc240908153e19a30a6444cb04a6
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2024-10-29 23:15:01 +0100

    New Pattern: Introduction.

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -2,6 +2,8 @@
 Here we collect the chatbot text patterns.
 """
 
+import datetime
+
 psychobabble = [
     [r"geht.{0,5}s.{0,5}dir",
     ["Danke. Mir geht es gut und dir?",
@@ -13,4 +15,10 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"Ich heiße (.*)",
+     ["Schön, dich kennenzulernen, {0}.",
+      "Hallo, {0}.",
+      "{0}? Schöner Name!"]]
+
 ]
+
