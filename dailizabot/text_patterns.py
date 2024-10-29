c6720b8894c7ca00e56e1c64c82787102faebf4e
AlJasmin <jasminalkarawani@outlook.de>
2024-10-29 13:07:22 +0100

    Pattern Nr.1

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -64,3 +64,6 @@ psychobabble.append((r'Was geht', ["nichts und bei dir?"]))
 
 
 psychobabble.append((r'ich mag (.*)', ["Warum magst du {0}?", "Was gefällt dir besonders an {0}?"]))
+
+
+psychobabble.append((r"Ich möchte (.*) lernen", ["Klingt gut! Was interessiert dich an {0}?"]))
