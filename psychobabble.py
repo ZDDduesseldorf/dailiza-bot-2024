6a03134d8316ad296fddc83c28a9e2279a39506c
Nevriye Kilicarslan <nevriye.kilicarslan@study.hs-duesseldorf.de>
2024-10-29 16:53:52 +0100

    Add first regex pattern for psychobabble

---
+++ psychobabble.py
@@ -0,0 +1,6 @@
+psychobabble_patterns = [
+    (r'Ich fühle mich (.*)', ['Warum fühlst du dich {}?']),
+    (r'Ich (kann|will|möchte) (.*)', ['Warum kannst du {} nicht?', 'Was würde passieren, wenn du {}?']),
+    (r'Was ist (.*)', ['Warum fragst du?']),
+    (r'Ich denke (.*)', ['Warum denkst du, dass {}?']),  # Füge hier dein erstes neues Muster hinzu
+]
