16162b8d81f839002f075a6cc8befa256608612d
Julia <julia.moor@study.hs-duesseldorf.de>
2024-10-30 01:19:35 +0100

    dailizabot/text_patterns.py

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -21,6 +21,11 @@ psychobabble = [
     [r"Was machst du (.*)",
     ["Ganz viele 0en und 1en schreiben. 01101001 01100011 01101000 00100000 01100010 01101001 01101110 00100000 01101101 11000011 10111100 01100100 01100101 00100000 00111100 00110011",
     "Das ist natürlich ein Geheimnis :)",
-    "Ich tu einfach so, als ob ich ein Chatbot wäre. Was machst du so?"]],
+    "Ich tu einfach so, als ob ich ein Chatbot wäre. Und du?"]],
+
+    [r"Wieso (.*)",
+    ["Na sowas! Genau das wollte ich dich auch fragen!",
+    "Das wirst du schon noch herausfinden.",
+    "Tja, das bleibt wohl mein kleines Bot-Geheimnis."]],
 
 ]
